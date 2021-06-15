3e3ee1bd3aa9c078c70c98f13890982c1d495f41
daeyeong1573 <57317043+daeyeong1573@users.noreply.github.com>
2021-06-15 21:11:17 +0900

    데이터 바인딩 연결

--- app/src/main/java/com/example/hakrim/view/fragment/MealFragment.kt
+++ app/src/main/java/com/example/hakrim/view/fragment/MealFragment.kt
@@ -1,4 +1,4 @@
-package com.example.hakrim
+package com.example.hakrim.view.fragment
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -7,19 +7,30 @@ import android.view.ViewGroup
     import androidx.fragment.app.Fragment
 import com.example.hakrim.databinding.FragmentInformationBinding
 import com.example.hakrim.databinding.FragmentMealBinding
+import com.example.hakrim.util.Time
 import java.lang.reflect.Array.get
 
 class MealFragment : Fragment() {
     private val binding by lazy { FragmentMealBinding.inflate(layoutInflater) }
+    lateinit var time : Time
+
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
 
-
-
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.time = Time()
+    }
+
+    fun mealSelect(){
+
     }
 
 }
